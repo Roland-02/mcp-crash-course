a93b051e0df0743013709f4c1a2a26d435e09af2
O Olajide <oo169@student.le.ac.uk>
2026-04-21 23:07:56 +0100

    agent

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 from dotenv import load_dotenv
 from langchain.agents import create_agent
+from langchain.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_mcp_adapters.tools import load_mcp_tools
 from mcp import StdioServerParameters, ClientSession
@@ -10,7 +11,7 @@ from mcp.client.stdio import stdio_client
 
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.9, max_tokens=2048)
+llm = ChatGoogleGenerativeAI(model="gemini-3.1-pro-preview", temperature=0.9, max_tokens=2048)
 
 stdio_server_params = StdioServerParameters(
     command="python",
@@ -19,6 +20,20 @@ stdio_server_params = StdioServerParameters(
 
 async def main():
     print("Hello from mcp-crash-course!")
+
+    async with stdio_client(stdio_server_params) as (read,write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Client session initialized")
+
+            tools = await load_mcp_tools(session)
+            print(f"Loaded tools: {tools}")
+
+            agent = create_agent(llm,tools)
+            print("Agent created")
+
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
 
 
 if __name__ == "__main__":
